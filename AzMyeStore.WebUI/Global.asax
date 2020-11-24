7ce8b70b44b966bf992da0407c4b247dd5196af9
azza9885 <49562774+azza9885@users.noreply.github.com>
2020-11-24 01:56:13 -0500

    Initial Solution Setup

---
+++ AzMyeStore.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzMyeStore.WebUI.MvcApplication" Language="C#" %>
---
+++ AzMyeStore.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzMyeStore.WebUI.Startup))]
+namespace AzMyeStore.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
